082a2c3cf09607c8038c2282e365a65012d47346
hoxuanphu <hoxuanphu.stmn@gmail.com>
2026-04-24 10:36:08 +0700

    Update quality_check.py

--- starter_code/quality_check.py
+++ starter_code/quality_check.py
@@ -6,10 +6,13 @@ def run_semantic_checks(doc_dict: dict) -> bool:
     content = doc_dict.get("content", "")
     
     # 1. Kiểm tra độ dài: Nếu content trống hoặc < 10 ký tự -> False
-    # TODO: Thực hiện kiểm tra độ dài ở đây
+    if not content or len(content) < 10:
+        return False
     
     # 2. Kiểm tra từ khóa lỗi
     toxic_keywords = ["Null pointer exception", "OCR Error", "Traceback"]
-    # TODO: Lặp qua các từ trong toxic_keywords, nếu từ đó xuất hiện trong content -> Trả về False
+    for keyword in toxic_keywords:
+        if keyword in content:
+            return False
             
     return True
